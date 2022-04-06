f8bf321c3acc87035f3dfe6b39567e4835c33dbc
Pavel Taruts <ptrtss@gmail.com>
2022-04-06 12:54:10 +0300

    Logging errors

--- src/main/java/we/processUtils/ProcessRunner.java
+++ src/main/java/we/processUtils/ProcessRunner.java
@@ -1,9 +1,12 @@
 package we.processUtils;
 
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.BufferedReader;
 import java.io.File;
 
+@Slf4j
 public class ProcessRunner {
 
     @SneakyThrows
@@ -11,9 +14,17 @@ public class ProcessRunner {
         ProcessBuilder processBuilder = new ProcessBuilder(command);
         processBuilder.directory(directory);
         Process process = processBuilder.start();
-        int exitStatus = process.waitFor();
-        if (exitStatus != 0) {
-            throw new RuntimeException(command[0] + " exited with status " + exitStatus);
+
+        try (
+                BufferedReader error = process.errorReader();
+                BufferedReader input = process.inputReader();
+        ) {
+            int exitStatus = process.waitFor();
+            if (exitStatus != 0) {
+                input.lines().forEachOrdered(log::info);
+                error.lines().forEachOrdered(log::error);
+                throw new RuntimeException(command[0] + " exited with status " + exitStatus);
+            }
         }
     }
 }
